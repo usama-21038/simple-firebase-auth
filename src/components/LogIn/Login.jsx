470db767c4a00a12b4a32d4d60f903f659de1a8d
Usama <shihabhasib777@gmail.com>
2026-02-04 01:48:06 +0600

    github auth

--- src/components/LogIn/Login.jsx
+++ src/components/LogIn/Login.jsx
@@ -1,11 +1,13 @@
-import React, { use } from 'react'; 
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
 import { useState } from 'react';
 const Login = () => {
     const [user,setUser]=useState(null);
     const provider=new GoogleAuthProvider();
+    const provider2 = new GithubAuthProvider();
 
+    // event handler for google sign in
     const handleGoogleSignIn = () => {
         console.log('Google sign-in clicked');
 
@@ -19,19 +21,56 @@ const Login = () => {
             console.log('Error during sign-in:', error);
         })
     }
+
+// event handler for sign out
+const handleSignOut=()=>{
+     signOut(auth).then(()=>{
+        console.log("SignOut completed");
+        setUser(null);
+    })
+    .catch(error=>{console.log(error)})
+}
+
+// event handler for github sign in
+const handleGithubSignIn=()=>{
+    console.log('Github sign-in clicked');
+
+    signInWithPopup(auth,provider2)
+    .then(result=>{
+        console.log('User signed in:', result);
+setUser(result.user);
+    })
+    .catch(error=>{
+console.log('Error during sign-in:', error);
+    })
+}
  
+
+
+
     return (
         <div>
             <h1>Pleace login</h1>
-            <button onClick={handleGoogleSignIn} className='p-3 bg-amber-200 m-5 text-black rounded-2xl hover:bg-amber-600 hover:text-white'>Sign in with google</button>
+            {/* <button onClick={handleGoogleSignIn} className='p-3 bg-amber-200 m-5 text-black rounded-2xl hover:bg-amber-600 hover:text-white'>Sign in with google</button> */}
+            {/* for sign out */}
+            {/* <button onClick={handleSignOut} className='p-3 bg-amber-200 m-5 text-black rounded-2xl hover:bg-amber-600 hover:text-white'>Sign Out</button> */}
+
+            {
+                //jodi user thake taile first "" ar jodi na thake taile second "" ar kaj korbe
+                user ? <button onClick={handleSignOut} className='p-3 bg-amber-200 m-5 text-black rounded-2xl hover:bg-amber-600 hover:text-white'>Sign Out</button> : <>
+                <button onClick={handleGoogleSignIn} className='p-3 bg-amber-200 m-5 text-black rounded-2xl hover:bg-amber-600 hover:text-white'>Sign in with google</button>
+                <button onClick={handleGithubSignIn} className='p-3 bg-amber-200 m-5 text-black rounded-2xl hover:bg-amber-600 hover:text-white'>Sign in with Github</button>
+
+                </>
+            }
 
             {
                 // if user then show user info
                 user && 
             <div>
-                <h3>{user?.displayName}</h3>
+                <h3>{user?.displayName}</h3> 
                 <p>{user?.email}</p>
-                <img src="{user.photoURL}" alt="" />
+                <img src={user?.photoURL} alt="" />
 
             </div>
             }
